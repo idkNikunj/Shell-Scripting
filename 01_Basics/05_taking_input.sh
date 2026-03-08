11b6183701463009e7d066beed904f9dc8dd6f37
Shakalen Shaikh <160219957+ShakalBhau0001@users.noreply.github.com>
2026-03-08 15:21:08 +0530

    Update 05_taking_input.sh

--- 01_Basics/05_taking_input.sh
+++ 01_Basics/05_taking_input.sh
@@ -1,1 +1,36 @@
+#!/bin/bash
+# Taking the input from the user
 
+# Syntax :
+
+# read var_name
+
+# OR
+
+# read -p "Message" var_name
+
+# Example :
+
+echo "Enter your name : "
+read name
+echo "Hello $name"
+
+# OR
+
+read -p "Enter your age : " age
+echo "Your age is $age"
+
+
+# Enter your name : 
+# Shakalen
+# Hello Shakalen
+# Enter your age : 21
+# Your age is 21
+
+
+printf "Hi My name is Shakalen\nI am a backend developer\nI am 21 years old\n"
+
+# Output =>
+# Hi My name is Shakalen
+# I am a backend developer
+# I am 21 years old

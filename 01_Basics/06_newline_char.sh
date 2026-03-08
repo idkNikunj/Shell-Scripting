5a60fea8c4ad2b21effa87f8d280999d94f2607d
Shakalen Shaikh <160219957+ShakalBhau0001@users.noreply.github.com>
2026-03-08 15:23:16 +0530

    Update 06_newline_char.sh

--- 01_Basics/06_newline_char.sh
+++ 01_Basics/06_newline_char.sh
@@ -1,1 +1,24 @@
+#!/bin/bash
 
+# By using printf command
+
+printf "Hi My name is Shakal\nI am a backend developer\nI am 19 years old\n"
+
+# Output =>
+# Hi My name is Shreyash
+# I am a backend developer
+# I am 19 years old
+
+echo -e "\n"
+
+# By using echo command with option -e
+
+echo -e "Hi My name is Shakal\nI am a backend developer\nI am 19 years old\n"
+
+# Output =>
+# Hi My name is Shakal
+# I am a backend developer
+# I am 19 years old
+
+
+echo -e "\n"

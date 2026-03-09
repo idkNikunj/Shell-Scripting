7f02b8175d2cede41e0e4ccb0d22835058ff6345
Shakalen Shaikh <160219957+ShakalBhau0001@users.noreply.github.com>
2026-03-09 14:31:33 +0530

    Update 04_variables_advanced.sh

--- 02_Variables/04_variables_advanced.sh
+++ 02_Variables/04_variables_advanced.sh
@@ -1,1 +1,25 @@
+#!/bin/bash
 
+name="Shakal"
+age=19
+gender='Male'
+
+echo "Name: $name"
+echo 'Name: $name'
+
+echo "Age: $age"
+
+echo "Gender: $gender"
+
+
+name=ABC        # Here variable name value is changed
+echo "Name: $name"
+
+
+# Variable to store the output of a command
+
+hostname=$(hostname)
+echo "Name of this machine is: $hostname"
+
+date=$(date)
+echo "Date is: $date"

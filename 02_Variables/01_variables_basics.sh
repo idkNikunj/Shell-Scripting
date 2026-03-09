941d6741361de562339d7112268d033174921dc6
Shakalen Shaikh <160219957+ShakalBhau0001@users.noreply.github.com>
2026-03-09 14:28:23 +0530

    Update 01_variables_basics.sh

--- 02_Variables/01_variables_basics.sh
+++ 02_Variables/01_variables_basics.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+int_var=10
+str_var="Shakal Bhau"
+float_var=10.5
+bool_var=true
 
+echo "Integer vaiable: $int_var"
+echo "String variable: $str_var"
+echo "Float variable: $float_var"
+echo "Bool variable: $bool_var"
+
+int_var=100
+
+echo "Integer variable after changing value: $int_var"
+
+
+a=100
+
+echo "$a"    # while printing the value of variable inside double quotes it can work 
+echo '$a'    # while printing the value of variable inside single quotes it can not work 

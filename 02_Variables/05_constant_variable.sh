9e602440bd3fe49e6e2c72bf0de86deb430a9111
Shakalen Shaikh <160219957+ShakalBhau0001@users.noreply.github.com>
2026-03-09 14:32:41 +0530

    Update 05_constant_variable.sh

--- 02_Variables/05_constant_variable.sh
+++ 02_Variables/05_constant_variable.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+# Constant variable in shell script
+
+readonly city="Pune"
+
+echo "My city name is: $city"
+
+city="Pune"      # Here error will be thrown because we have used readonly keyword or it an constant
+# echo "My city name is: $city"

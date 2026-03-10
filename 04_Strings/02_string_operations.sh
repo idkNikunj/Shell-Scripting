cdd635bfe82051a1837d33fb6030286014f1b7d1
Shakalen Shaikh <160219957+ShakalBhau0001@users.noreply.github.com>
2026-03-11 02:07:35 +0530

    Update 02_string_operations.sh

--- 04_Strings/02_string_operations.sh
+++ 04_Strings/02_string_operations.sh
@@ -1,1 +1,71 @@
+#!/bin/bash
+# String Operatiobns
+
+str="Hello, I am Shakalen"
+
+
+# Find the length of string :
+
+# Syntax :
+# ${#str_name}
+# OR
+# ` expr length "$str_name" `
+
+
+echo "Length of String is: ${#str}"         # Length of String is: 20
+# OR
+echo "Length of string is: "`expr length "$str"`      # Length of string is: 20
+
+
+# To make the string uppercase :
+
+# Syntax :
+# ${str_name^^}
+
+upper=${str^^}          # ^^ This can make the string to upper case
+
+echo $upper             # HELLO, I AM SHAKALEN
+
+echo "Upper case of $str is:  ${str^^}"     # Upper case of Hello, I am Shakalen is:  HELLO, I AM SHAKALEN
+
+
+# To make the string lowercase :
+
+# Syntax :
+# ${str_name,,}
+
+lower=${str,,}           # ,, This can make the string to lower case
+
+echo $lower             # hello, i am shakalen
+
+
+# Replace a string words:
+
+# Syntax :
+# ${str_name/word_which_we_want_to_repace/word_which_we_want_to_insert}
+
+
+
+replace=${str/Hello/Hi}
+
+echo "Original string ----- $str"
+echo "After replacing ----- $replace"
+
+# Output =>
+# Original string ----- Hello, I am Shakalen
+# After replacing ----- Hi, I am Shakalen
+
+
+# Slicing
+
+# Syntax :
+# ${str_name:start:end}
+
+
+name=${str:12}
+
+echo "Name: $name"         # Name: Shakalen
+
+sub=`expr substr "$str" 12 20`
+echo "Substr: $sub"        # Substr:  Shakalen
 

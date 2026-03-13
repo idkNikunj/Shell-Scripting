fc764a4ab5d2b9fa3c8a98665c78b41519f6d0b1
Shakalen Shaikh <160219957+ShakalBhau0001@users.noreply.github.com>
2026-03-13 12:09:30 +0530

    Update 01_if_statement.sh

--- 05_Conditionals/01_if_statement.sh
+++ 05_Conditionals/01_if_statement.sh
@@ -1,1 +1,34 @@
+#!/bin/bash
 
+# 1) IF statement
+
+# Syntax:
+
+# if [ condition ] # condition can be any logical expression
+# then
+#     # code to be executed if condition is true
+# fi
+
+# Example:
+
+read -p "Enter any number: " num
+if [ $num -eq 10 ]
+then
+    echo "Number is equal to 10"
+fi
+
+# Output =>
+# Enter any number: 10
+# Number is equal to 10
+
+
+
+num=10
+
+if [ $num -gt 5 ]; then
+    echo "Number is greater than 5."
+fi
+
+
+# Output =>
+# Number is greater than 5.
